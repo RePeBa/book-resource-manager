2de2f4efe17b8d243420b990f6c30e184bd09637
stbanas <pio.banasik@gmail.com>
2021-07-16 11:34:36 +0200

    add user table

--- utils/db-init.py
+++ utils/db-init.py
@@ -1,12 +1,14 @@
 import sqlite3
 
-conn = sqlite3.connect('../books.db')
+conn = sqlite3.connect('../dbase.db')
 
 c = conn.cursor()
 
 c.execute("""CREATE TABLE IF NOT EXISTS book( id integer primary key, author text, title text) """)
+c.execute("""CREATE TABLE IF NOT EXISTS user( id integer primary key, username text, email text, password text, user_id integer) """)
 
 c.execute("SELECT * FROM book")
+c.execute("SELECT * FROM user")
 
 print(c.fetchall())
 
